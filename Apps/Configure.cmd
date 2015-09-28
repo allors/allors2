1067fcb067130ead1aaafe09ceca8359eca67092
Koen Van Exem <koen@vanexem.be>
2015-09-28 09:55:26 +0200

    configure.cmd from vs2013 -> vs2015

--- Apps/Configure.cmd
+++ Apps/Configure.cmd
@@ -1,4 +1,4 @@
-@SET PATH=%PATH%;C:\Program Files\MSBuild\12.0\Bin;C:\Program Files (x86)\MSBuild\12.0\Bin;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+@SET PATH=%PATH%;C:\Program Files\MSBuild\14.0\Bin;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
 @msbuild Apps.sln /target:Clean /verbosity:minimal
 
