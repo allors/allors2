706821e973b3fcad92ed3566fa62feae9946127f
Koen Van Exem <koen@vanexem.be>
2017-01-23 18:10:33 +0100

    fixed adapters

--- Platform/Adapters/Configure.cmd
+++ Platform/Adapters/Configure.cmd
@@ -1,6 +1,6 @@
 @SET PATH=%PATH%;C:\Program Files\MSBuild\14.0\Bin;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
-..\Tools\dist\Allors.Tools.Cmd.exe repository generate adapters.sln repository ../base/templates/meta.cs.stg meta/generated
+..\..\Tools\dist\Allors.Tools.Cmd.exe repository generate adapters.sln repository ../../base/repository/templates/meta.cs.stg meta/generated
 
 @if NOT ["%errorlevel%"]==["0"] (
     pause
