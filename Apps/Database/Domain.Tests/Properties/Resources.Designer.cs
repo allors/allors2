5af2fc39b17abf338d959068111a611833310801
Koen Van Exem <koen@vanexem.be>
2017-02-07 17:45:12 +0100

    removed server

--- Apps/Database/Domain.Tests/Properties/Resources.Designer.cs
+++ Apps/Database/Domain.Tests/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tests.Properties {
+namespace Domain.Tests.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Tests.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Domain.Tests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
