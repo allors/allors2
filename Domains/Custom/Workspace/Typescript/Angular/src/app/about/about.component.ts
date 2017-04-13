f03ffa59c4313a21a7d1814402c6dcaa86c93298
Koen Van Exem <koen@vanexem.be>
2017-04-13 19:04:42 +0200

    angular

--- Domains/Custom/Workspace/Typescript/Angular/src/app/about/about.component.ts
+++ Domains/Custom/Workspace/Typescript/Angular/src/app/about/about.component.ts
@@ -1,32 +1,34 @@
+import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 import { Scope } from '../../angular/base/Scope';
-import { AllorsService } from "../allors.service";
 
-import { UnitSample } from "../../domain"
+import { AllorsService } from '../allors.service';
+
+import { UnitSample } from '../../domain';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent extends Scope implements OnInit {
+export class AboutComponent implements OnInit {
 
+  scope: Scope;
   unitSample: UnitSample;
 
-  constructor(allors: AllorsService) 
-  { 
-    super("TestUnitSamples", allors.database, allors.workspace);
+  constructor(allors: AllorsService) {
+    this.scope = new Scope('TestUnitSamples', allors.database, allors.workspace);
   }
 
   ngOnInit() {
-    this.refresh();
+    this.refresh().subscribe();
   }
 
-  protected refresh(): Promise<any> {
-    return this.load({Step: 1})
-        .then(() => {
-
-            this.unitSample = this.context.objects["unitSample"] as UnitSample;
+  protected refresh(): Observable<any> {
+    return this.scope
+        .load({Step: 1})
+        .do(() => {
+            this.unitSample = this.scope.objects['unitSample'] as UnitSample;
         });
   }
 }
