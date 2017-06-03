bba6aa8fa4d14fb03ee33931afe5d9acac2fad85
Koen Van Exem <koen@vanexem.be>
2017-06-03 10:54:05 +0200

    Query

---
+++ Domains/Base/Database/Domain/Base/Query/ISessionExtensions.cs
@@ -0,0 +1,33 @@
+//-------------------------------------------------------------------------------------------------
+// <copyright file="ISessionExtensions.cs" company="Allors bvba">
+// Copyright 2002-2017 Allors bvba.
+//
+// Dual Licensed under
+//   a) the General Public Licence v3 (GPL)
+//   b) the Allors License
+//
+// The GPL License is included in the file gpl.txt.
+// The Allors License is an addendum to your contract.
+//
+// Allors Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// For more information visit http://www.allors.com/legal
+// </copyright>
+// <summary>Defines the ISessionExtension type.</summary>
+//-------------------------------------------------------------------------------------------------
+
+namespace Allors
+{
+    using Allors.Domain.Query;
+
+    public static partial class ISessionExtensions
+    {
+        public static Extent Query(this ISession @this, Query query)
+        {
+            return query.Build(@this);
+        }
+    }
+}
