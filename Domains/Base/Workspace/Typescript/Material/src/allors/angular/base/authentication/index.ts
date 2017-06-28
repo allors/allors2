0deebef540a5468e60554add0b57ab125ed09cf3
Koen Van Exem <koen@vanexem.be>
2017-06-28 11:48:29 +0200

    material

---
+++ Domains/Base/Workspace/Typescript/Material/src/allors/angular/base/authentication/index.ts
@@ -0,0 +1,3 @@
+﻿export { AuthenticationService } from './authentication.service';
+
+
---
+++ Domains/Base/Workspace/Typescript/Material/src/allors/domain/base/database/query/ContainedIn.ts
@@ -0,0 +1,25 @@
+﻿import { AssociationType, RoleType } from '../../../../meta';
+import { ISessionObject } from '../../workspace';
+import { Predicate } from './Predicate';
+import { Query } from './Query';
+
+export class ContainedIn implements Predicate {
+  associationType: AssociationType;
+  roleType: RoleType;
+  query: Query;
+  objects: ISessionObject[];
+
+  constructor(fields?: Partial<ContainedIn>) {
+    Object.assign(this, fields);
+  }
+
+  toJSON(): any {
+    return {
+      _T: 'ContainedIn',
+      at: this.associationType ? this.associationType.id : undefined,
+      rt: this.roleType.id ? this.roleType.id : undefined,
+      q: this.query,
+      o: this.objects ? this.objects.map((v: ISessionObject) => v.id) : undefined,
+    };
+  }
+}
