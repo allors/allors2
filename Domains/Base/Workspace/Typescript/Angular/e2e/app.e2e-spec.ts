eea195b906d06ce8840ca61e6167031afd20e328
Koen Van Exem <koen@vanexem.be>
2017-08-09 14:47:41 +0200

    base ws angular - teamcity

--- Domains/Base/Workspace/Typescript/Angular/e2e/app.e2e-spec.ts
+++ Domains/Base/Workspace/Typescript/Angular/e2e/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('angular App', () => {
     page.login('john@doe.org', 'john');
   });
 
-  it('should have title Home', () => {
+  it('should have title Home', async () => {
     page.navigateTo();
-
-    expect('Home').toEqual(page.title);
+    const title = await page.title;
+    expect('Angular').toEqual(title);
   });
 });
--- Domains/Base/Workspace/Typescript/Angular/karma.conf.js
+++ Domains/Base/Workspace/Typescript/Angular/karma.conf.js
@@ -8,15 +8,17 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('karma-teamcity-reporter'),
+      require('@angular/cli/plugins/karma'),
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
