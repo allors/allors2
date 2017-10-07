c601941cc89c663e886ec523a2eaeaed9da86164
martienvanknippenberg <martien.vanknippenberg@inxin.com>
2017-10-07 14:40:06 +0200

    fix singletons

--- Domains/Apps/Database/Domain/Apps/Common/Singletons.cs
+++ Domains/Apps/Database/Domain/Apps/Common/Singletons.cs
@@ -4,12 +4,12 @@
 
     public partial class Singletons
     {
-        protected override void CustomPrepare(Setup config)
+        protected override void AppsPrepare(Setup config)
         {
             config.AddDependency(this.ObjectType, M.Locale.ObjectType);
         }
 
-        protected override void CustomSetup(Setup setup)
+        protected override void AppsSetup(Setup setup)
         {
             var singleton = this.Instance;
             singleton.DefaultLocale = new Locales(this.Session).DutchBelgium;
