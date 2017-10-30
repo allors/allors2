ee3e47bd195074bdf8c03ee893afa538ffa2b442
Koen Van Exem <koen@vanexem.be>
2017-10-30 16:18:37 +0100

    angular teamcity test

--- Domains/Base/Workspace/Typescript/Angular/src/app/app.component.spec.ts
+++ Domains/Base/Workspace/Typescript/Angular/src/app/app.component.spec.ts
@@ -1,22 +1,24 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { async, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
+      ],
+      imports: [
+        RouterTestingModule,
       ],
     }).compileComponents();
   }));
-  // it('should create the app', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // }));
+
+  it("should create the app", async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
   // it(`should have as title 'app'`, async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.debugElement.componentInstance;
