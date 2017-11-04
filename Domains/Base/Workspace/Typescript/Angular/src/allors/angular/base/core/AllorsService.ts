ddabb647388e1cd61e2f12cb758652ee84ad8bf7
martienvanknippenberg <martien.vanknippenberg@inxin.com>
2017-11-04 13:24:51 +0100

    fix

--- Domains/Base/Workspace/Typescript/Angular/src/allors/angular/base/core/AllorsService.ts
+++ Domains/Base/Workspace/Typescript/Angular/src/allors/angular/base/core/AllorsService.ts
@@ -5,7 +5,7 @@ import { Database } from "./Database";
 export abstract class AllorsService {
   public abstract workspace: Workspace;
   public abstract database: Database;
-  public abstract m: MetaDomain;
+  public abstract meta: MetaDomain;
 
   public abstract back();
 }
