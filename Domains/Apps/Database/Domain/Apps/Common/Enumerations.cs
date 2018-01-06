c5ba45b5149b7777055eccad50350dc2d3aa80c1
martienvanknippenberg <martien.vanknippenberg@inxin.com>
2018-01-06 12:57:35 +0100

    setup  enumerations is dependent upon Singleton

---
+++ Domains/Apps/Database/Domain/Apps/Common/Enumerations.cs
@@ -0,0 +1,12 @@
+﻿namespace Allors.Domain
+{
+    using Allors.Meta;
+
+    public partial class Enumerations
+    {
+        protected override void AppsPrepare(Setup config)
+        {
+            config.AddDependency(this.ObjectType, M.Singleton.ObjectType);
+        }
+    }
+}
