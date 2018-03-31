d2614e3ed021407e768b0acd4e900994057f86bd
Koen Van Exem <koen@vanexem.be>
2018-03-31 16:51:40 +0200

    protractor timeout

--- Domains/Apps/Workspace/Typescript/Intranet/protractor.conf.teamcity.js
+++ Domains/Apps/Workspace/Typescript/Intranet/protractor.conf.teamcity.js
@@ -14,7 +14,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 2147483647, // 2^31-1
     print: function () { }
   },
   onPrepare() {
