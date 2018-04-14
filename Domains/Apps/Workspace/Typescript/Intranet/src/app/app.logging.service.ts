618da98dca3bfe9e29634c33fd1028738da8c217
Koen Van Exem <koen@vanexem.be>
2018-04-14 13:46:32 +0200

    angular logging

--- Domains/Apps/Workspace/Typescript/Intranet/src/app/app.logging.service.ts
+++ Domains/Apps/Workspace/Typescript/Intranet/src/app/app.logging.service.ts
@@ -6,9 +6,25 @@ import { environment } from "../environments/environment";
 
 import { v4 as uuid } from "uuid";
 
+function dump(o: any){
+  var cache = [];
+  var result = JSON.stringify(o, (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+          if (cache.indexOf(value) !== -1) {
+              return;
+          }
+          cache.push(value);
+      }
+      
+      return value;
+  });
+
+  cache = null;
+  return result;
+}
+
 @Injectable()
 export class DefaultLoggingService extends LoggingService {
-
   private logger: JL.JSNLogLogger;
 
   constructor() {
@@ -18,34 +34,37 @@ export class DefaultLoggingService extends LoggingService {
 
     JL.setOptions({
       defaultAjaxUrl: environment.url + "logging",
-      requestId,
+      requestId
     });
 
-    const level = environment.production ? JL.getWarnLevel() : JL.getDebugLevel();
-    this.logger = JL("Intranet").setOptions({level});
+    const level = environment.production
+      ? JL.getWarnLevel()
+      : JL.getDebugLevel();
+    this.logger = JL("Intranet").setOptions({ level });
   }
 
   public trace(logObject: any) {
-    this.logger.trace(logObject);
+    this.logger.trace(dump(logObject));
   }
 
   public debug(logObject: any) {
-    this.logger.debug(logObject);
+    this.logger.debug(dump(logObject));
   }
 
   public info(logObject: any) {
-    this.logger.info(logObject);
+    this.logger.info(dump(logObject));
   }
 
   public warn(logObject: any) {
-    this.logger.warn(logObject);
+    this.logger.warn(dump(logObject));
   }
 
   public error(logObject: any) {
-    this.logger.error(logObject);
+    var x = dump(logObject);
+    this.logger.error(dump(logObject));
   }
 
   public fatal(logObject: any) {
-    this.logger.fatal(logObject);
+    this.logger.fatal(dump(logObject));
   }
 }
