b89f13e0a1acd94e75eaf342681a5203e25cafc1
Koen Van Exem <koen@vanexem.be>
2018-08-06 17:49:06 +0200

    query api

---
+++ Platform/Database/Framework/Allors.Framework/Query/GreaterThan.cs
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------------------------- 
+// <copyright file="Extent.cs" company="Allors bvba">
+// Copyright 2002-2017 Allors bvba.
+// 
+// Dual Licensed under
+//   a) the Lesser General Public Licence v3 (LGPL)
+//   b) the Allors License
+// 
+// The LGPL License is included in the file lgpl.txt.
+// The Allors License is an addendum to your contract.
+// 
+// Allors Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// For more information visit http://www.allors.com/legal
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Allors.Query
+{
+    public class GreaterThan : IPredicate
+    {
+        public Guid Operand { get; set; }
+
+        public string Value { get; set; }
+    }
+}
