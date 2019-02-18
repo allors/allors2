941c9c8c3567a2caa6025d8cd5d2a0701cd953cb
martienvanknippenberg <martien.vanknippenberg@inxin.com>
2019-02-18 14:58:38 +0100

    custom setup PositionType

--- Domains/Apps/Repository/Domain/Export/Apps/WorkEffortPartyAssignment.cs
+++ Domains/Apps/Repository/Domain/Export/Apps/WorkEffortPartyAssignment.cs
@@ -7,9 +7,13 @@ namespace Allors.Repository
     #region Allors
     [Id("0bdfb093-35af-4c87-9c1c-05ed9dae6df6")]
     #endregion
-    public partial class WorkEffortPartyAssignment : AccessControlledObject, Commentable 
+    public partial class WorkEffortPartyAssignment : Period, AccessControlledObject, Commentable 
     {
         #region inherited properties
+
+        public DateTime FromDate { get; set; }
+        public DateTime ThroughDate { get; set; }
+
         public Permission[] DeniedPermissions { get; set; }
 
         public SecurityToken[] SecurityTokens { get; set; }
@@ -43,24 +47,6 @@ namespace Allors.Repository
         public Party Party { get; set; }
 
         #region Allors
-        [Id("3F3D9387-0758-4559-B33F-0C7B352B171C")]
-        [AssociationId("EAF539A6-4427-4BC1-B65E-F18E6B86CC08")]
-        [RoleId("FD4E4258-8BA2-44D3-92ED-876AB7084458")]
-        #endregion
-        [Indexed]
-        [Workspace]
-        public DateTime FromDate { get; set; }
-
-        #region Allors
-        [Id("2A49EA68-DB8F-4186-9D7E-FE2CC1AFD6F5")]
-        [AssociationId("126CDB1A-46F3-4C20-B93F-D753F6A5CD29")]
-        [RoleId("DE319E9D-2814-4656-832A-8A4876FC08D3")]
-        #endregion
-        [Indexed]
-        [Workspace]
-        public DateTime ThroughDate { get; set; }
-
-        #region Allors
         [Id("f88ae79d-7605-4be9-972e-541489bdb72b")]
         [AssociationId("b527fed9-c720-45aa-b8c5-fe5336f43f5c")]
         [RoleId("92716fbf-aae3-433f-85f1-9fcfeb68568c")]
@@ -69,6 +55,16 @@ namespace Allors.Repository
         [Indexed]
         [Workspace]
         public Facility Facility { get; set; }
+
+        #region Allors
+        [Id("A5E053FB-957D-49A4-8EFD-D918043BEDBA")]
+        [AssociationId("31B8B332-C2C9-4432-AE64-AFEE4571CDDF")]
+        [RoleId("07F29231-3F75-4D3E-B803-5A372A896A2B")]
+        [Indexed]
+        #endregion
+        [Multiplicity(Multiplicity.OneToMany)]
+        [Workspace]
+        public WorkEffortAssignmentRate[] AssignmentRates { get; set; }
 
         #region inherited methods
 
---
+++ Domains/Apps/Workspace/Typescript/Intranet/src/allors/material/apps/objects/workeffortassignmentrate/overview/panel/workeffortassignmentrate-overview-panel.component.ts
@@ -0,0 +1,149 @@
+import { Component, Self, OnInit, HostBinding } from '@angular/core';
+import { PanelService, NavigationService, RefreshService, ErrorService, Action, MetaService } from '../../../../../../angular';
+import { WorkEffortAssignmentRate } from '../../../../../../domain';
+import { Meta } from '../../../../../../meta';
+import { DeleteService, TableRow, Table, CreateData, EditService, EditData } from '../../../../..';
+import * as moment from 'moment';
+
+interface Row extends TableRow {
+  object: PartyRate;
+  rateType: string;
+  from: string;
+  through: string;
+  rate: number;
+  frequency: string;
+}
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'workeffortassignmentrate-overview-panel',
+  templateUrl: './workeffortassignmentrate-overview-panel.component.html',
+  providers: [PanelService]
+})
+export class WorkEffortAssignmentRateOverviewPanelComponent implements OnInit {
+
+  @HostBinding('class.expanded-panel') get expandedPanelClass() {
+    return this.panel.isExpanded;
+  }
+
+  m: Meta;
+
+  objects: PartyRate[];
+  table: Table<Row>;
+
+  delete: Action;
+  edit: Action;
+
+  get createData(): CreateData {
+    return {
+      associationId: this.panel.manager.id,
+      associationObjectType: this.panel.manager.objectType,
+    };
+  }
+
+  collection = 'Current';
+  currentPartyRates: PartyRate[];
+  inactivePartyRates: PartyRate[];
+  allPartyRates: PartyRate[] = [];
+
+  constructor(
+    @Self() public panel: PanelService,
+    public metaService: MetaService,
+    public refreshService: RefreshService,
+    public navigationService: NavigationService,
+    public errorService: ErrorService,
+    public deleteService: DeleteService,
+    public editService: EditService
+  ) {
+
+    this.m = this.metaService.m;
+  }
+
+  ngOnInit() {
+
+    this.panel.name = 'partyrate';
+    this.panel.title = 'Party Rates';
+    this.panel.icon = 'contacts';
+    this.panel.expandable = true;
+
+    this.delete = this.deleteService.delete(this.panel.manager.context);
+    this.edit = this.editService.edit();
+
+    const sort = true;
+    this.table = new Table({
+      selection: true,
+      columns: [
+        { name: 'rateType' },
+        { name: 'from', sort },
+        { name: 'through', sort },
+        { name: 'rate', sort },
+        { name: 'frequency' },
+      ],
+      actions: [
+        this.edit,
+        this.delete,
+      ],
+      defaultAction: this.edit,
+      autoSort: true,
+      autoFilter: true,
+    });
+
+    const pullName = `${this.panel.name}_${this.m.PartyRate.name}`;
+
+    this.panel.onPull = (pulls) => {
+
+      const { pull, x, tree } = this.metaService;
+      const id = this.panel.manager.id;
+
+      pulls.push(
+        pull.Party({
+          name: pullName,
+          object: id,
+          fetch: {
+            PartyRates: {
+              include: {
+                RateType: x,
+                Frequency: x
+              }
+            }
+          }
+        }),
+      );
+    };
+
+    this.panel.onPulled = (loaded) => {
+      this.objects = loaded.collections[pullName] as PartyRate[];
+
+      if (this.objects) {
+        this.table.total = loaded.values[`${pullName}_total`] || this.objects.length;
+        this.refreshTable();
+      }
+    };
+  }
+
+  public refreshTable() {
+    this.table.data = this.partyRates.map((v) => {
+      return {
+        object: v,
+        rateType: v.RateType.Name,
+        from: moment(v.FromDate).format('L'),
+        through: v.ThroughDate !== null ? moment(v.ThroughDate).format('L') : '',
+        rate: v.Rate,
+        frequency: v.Frequency.Name,
+      } as Row;
+    });
+  }
+
+  get partyRates(): any {
+
+    switch (this.collection) {
+      case 'Current':
+        return this.objects && this.objects.filter(v => moment(v.FromDate).isBefore(moment()) && (!v.ThroughDate || moment(v.ThroughDate).isAfter(moment())));
+      case 'Inactive':
+        return this.objects && this.objects.filter(v => moment(v.FromDate).isAfter(moment()) || (v.ThroughDate && moment(v.ThroughDate).isBefore(moment())));
+      case 'All':
+      default:
+        return this.objects;
+    }
+  }
+}
