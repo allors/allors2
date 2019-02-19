91701b1e167dceb8b4d6e96cccc2b75fadf16bac
martienvanknippenberg <martien.vanknippenberg@inxin.com>
2019-02-19 15:42:41 +0100

    WorkEffortInventoryAssignment

---
+++ Domains/Apps/Workspace/Typescript/Intranet/src/allors/material/apps/objects/workeffortinventoryassignment/edit/workeffortinventoryassignment-edit.component.ts
@@ -0,0 +1,144 @@
+import { Component, OnDestroy, OnInit, Self, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { Subscription, combineLatest } from 'rxjs';
+
+import { ErrorService, ContextService, MetaService, RefreshService } from '../../../../../angular';
+import { WorkEffortPartyAssignment, Person, WorkEffort, Party } from '../../../../../domain';
+import { PullRequest, Sort } from '../../../../../framework';
+import { Meta } from '../../../../../meta';
+import { StateService } from '../../../services/state';
+import { switchMap, map } from 'rxjs/operators';
+import { EditData, CreateData, ObjectData } from 'src/allors/material/base/services/object';
+
+@Component({
+  templateUrl: './workeffortinventoryassignment-edit.component.html',
+  providers: [ContextService]
+})
+export class WorkEffortInventoryAssignmentEditComponent implements OnInit, OnDestroy {
+
+  readonly m: Meta;
+
+  workEffortPartyAssignment: WorkEffortPartyAssignment;
+  people: Person[];
+  workEfforts: WorkEffort[];
+  person: Person;
+  party: Party;
+  workEffort: WorkEffort;
+  assignment: WorkEffort;
+  contacts: Person[] = [];
+  title: string;
+
+  private subscription: Subscription;
+
+  constructor(
+    @Self() private allors: ContextService,
+    @Inject(MAT_DIALOG_DATA) public data: CreateData & EditData,
+    public dialogRef: MatDialogRef<WorkEffortInventoryAssignmentEditComponent>,
+    public metaService: MetaService,
+    public refreshService: RefreshService,
+    private errorService: ErrorService,
+    private stateService: StateService) {
+
+    this.m = this.metaService.m;
+  }
+
+  public ngOnInit(): void {
+
+    const { m, pull, x } = this.metaService;
+
+    this.subscription = combineLatest(this.refreshService.refresh$, this.stateService.internalOrganisationId$)
+      .pipe(
+        switchMap(([]) => {
+
+          const isCreate = (this.data as EditData).id === undefined;
+
+          const pulls = [
+            pull.WorkEffortPartyAssignment({
+              object: this.data.id,
+              include: {
+                Assignment: x,
+                Party: x
+              }
+            }),
+            pull.Party({
+              object: this.data.associationId
+            }),
+            pull.WorkEffort({
+              object: this.data.associationId
+            }),
+            pull.WorkEffort({
+              sort: new Sort(m.WorkEffort.Name)
+            }),
+            pull.Person({
+              sort: new Sort(m.Person.PartyName)
+            }),
+          ];
+
+          return this.allors.context
+            .load('Pull', new PullRequest({ pulls }))
+            .pipe(
+              map((loaded) => ({ loaded, isCreate }))
+            );
+        })
+      )
+      .subscribe(({ loaded, isCreate }) => {
+
+        this.allors.context.reset();
+
+        this.workEfforts = loaded.collections.WorkEfforts as WorkEffort[];
+        this.people = loaded.collections.People as Person[];
+        this.party = loaded.objects.Party as Party;
+        this.workEffort = loaded.objects.WorkEffort as WorkEffort;
+
+        if (isCreate) {
+          this.title = 'Add Work Effort Assignment';
+
+          this.workEffortPartyAssignment = this.allors.context.create('WorkEffortPartyAssignment') as WorkEffortPartyAssignment;
+
+          if (this.party !== undefined && this.party.objectType.name === m.Person.name) {
+            this.person = this.party as Person;
+            this.workEffortPartyAssignment.Party = this.person;
+          }
+
+          if (this.workEffort !== undefined && this.workEffort.objectType.name === m.WorkTask.name) {
+            this.assignment = this.workEffort as WorkEffort;
+            this.workEffortPartyAssignment.Assignment = this.assignment;
+          }
+
+        } else {
+          this.workEffortPartyAssignment = loaded.objects.WorkEffortPartyAssignment as WorkEffortPartyAssignment;
+          this.person = this.workEffortPartyAssignment.Party as Person;
+          this.assignment = this.workEffortPartyAssignment.Assignment;
+
+          if (this.workEffortPartyAssignment.CanWriteFromDate) {
+            this.title = 'Edit Work Effort Assignment';
+          } else {
+            this.title = 'View Work Effort Assignment';
+          }
+        }
+      }, this.errorService.handler);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  public save(): void {
+
+    this.allors.context.save()
+      .subscribe(() => {
+        const data: ObjectData = {
+          id: this.workEffortPartyAssignment.id,
+          objectType: this.workEffortPartyAssignment.objectType,
+        };
+
+        this.dialogRef.close(data);
+      },
+        (error: Error) => {
+          this.errorService.handle(error);
+        });
+  }
+}
---
+++ Domains/Apps/Workspace/Typescript/Intranet/src/allors/material/apps/objects/workeffortinventoryassignment/overview/panel/workeffortinventoryassignment-overview-panel.component.ts
@@ -0,0 +1,160 @@
+import { Component, Self, OnInit, HostBinding } from '@angular/core';
+import { NavigationService, Action, PanelService, RefreshService, ErrorService, MetaService } from '../../../../../../angular';
+import { WorkEffortPartyAssignment, OrganisationContactRelationship, Party } from '../../../../../../domain';
+import { Meta } from '../../../../../../meta';
+import { DeleteService, TableRow, EditService, Table, OverviewService, CreateData } from '../../../../..';
+import * as moment from 'moment';
+
+interface Row extends TableRow {
+  object: WorkEffortPartyAssignment;
+  number: string;
+  name: string;
+  status: string;
+  party: string;
+  from: string;
+  through: string;
+}
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'workeffortinventoryassignment-overview-panel',
+  templateUrl: './workeffortinventoryassignment-overview-panel.component.html',
+  providers: [PanelService]
+})
+export class WorkEffortInventoryAssignmentOverviewPanelComponent implements OnInit {
+
+  @HostBinding('class.expanded-panel') get expandedPanelClass() {
+    return this.panel.isExpanded;
+  }
+
+  m: Meta;
+
+  objects: WorkEffortPartyAssignment[] = [];
+
+  delete: Action;
+  edit: Action;
+  table: Table<TableRow>;
+
+  get createData(): CreateData {
+    return {
+      associationId: this.panel.manager.id,
+      associationObjectType: this.panel.manager.objectType,
+    };
+  }
+
+  constructor(
+    @Self() public panel: PanelService,
+    public metaService: MetaService,
+    public refreshService: RefreshService,
+    public navigation: NavigationService,
+    public errorService: ErrorService,
+    public deleteService: DeleteService,
+    public editService: EditService,
+  ) {
+
+    this.m = this.metaService.m;
+  }
+
+  ngOnInit() {
+
+    this.delete = this.deleteService.delete(this.panel.manager.context);
+    this.edit = this.editService.edit();
+
+    this.panel.name = 'workeffortpartyassignment';
+    this.panel.title = 'Work Effort Party Assignment';
+    this.panel.icon = 'work';
+    this.panel.expandable = true;
+
+    const sort = true;
+    this.table = new Table({
+      selection: true,
+      columns: [
+        { name: 'number', sort },
+        { name: 'name', sort },
+        { name: 'state', sort },
+        { name: 'party', sort },
+        { name: 'from', sort },
+        { name: 'through', sort },
+      ],
+      actions: [
+        this.edit,
+        this.delete,
+      ],
+      defaultAction: this.edit,
+      autoSort: true,
+      autoFilter: true,
+    });
+
+    const partypullName = `${this.panel.name}_${this.m.WorkEffortPartyAssignment.name}_party`;
+    const workeffortpullName = `${this.panel.name}_${this.m.WorkEffortPartyAssignment.name}_workeffort`;
+
+    this.panel.onPull = (pulls) => {
+      const { pull, x } = this.metaService;
+
+      const id = this.panel.manager.id;
+
+      pulls.push(
+        pull.Person({
+          name: partypullName,
+          object: id,
+          fetch: {
+            WorkEffortPartyAssignmentsWhereParty: {
+              include: {
+                Party: x,
+                Assignment: {
+                  WorkEffortState: x,
+                  Priority: x,
+                }
+              }
+            }
+          }
+        }),
+        pull.WorkEffort({
+          name: workeffortpullName,
+          object: id,
+          fetch: {
+            WorkEffortPartyAssignmentsWhereAssignment: {
+              include: {
+                Party: x,
+                Assignment: {
+                  WorkEffortState: x,
+                  Priority: x,
+                }
+              }
+            }
+          }
+        }),
+      );
+    };
+
+    this.panel.onPulled = (loaded) => {
+      const fromParty = loaded.collections[partypullName] as WorkEffortPartyAssignment[];
+      const fromWorkEffort = loaded.collections[workeffortpullName] as WorkEffortPartyAssignment[];
+
+      if (fromParty !== undefined && fromParty.length > 0) {
+        this.objects = fromParty;
+      }
+
+      if (fromWorkEffort !== undefined && fromWorkEffort.length > 0) {
+        this.objects = fromWorkEffort;
+      }
+
+      this.objects = fromParty || fromWorkEffort;
+
+      if (this.objects) {
+        this.table.total = this.objects.length;
+        this.table.data = this.objects.map((v) => {
+          return {
+            object: v,
+            number: v.Assignment.WorkEffortNumber,
+            name: v.Assignment.Name,
+            party: v.Party.displayName,
+            status: v.Assignment.WorkEffortState ? v.Assignment.WorkEffortState.Name : '',
+            from: moment(v.FromDate).format('L'),
+            through: v.ThroughDate !== null ? moment(v.ThroughDate).format('L') : '',
+          } as Row;
+        });
+      }
+    };
+  }
+}
