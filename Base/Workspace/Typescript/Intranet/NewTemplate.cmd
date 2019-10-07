c95ce8f04995d6662c6e8be9751d0e48eaf8460e
Koen Van Exem <koen@vanexem.be>
2019-10-07 13:07:06 +0200

    update angular material to latest versions

---
+++ Base/Workspace/Typescript/Intranet/NewTemplate.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+cd \temp
+ng new intranet --routing=true --style=scss
+cd intranet
+ng add @angular/material
---
+++ Core/Workspace/Typescript/Material/NewTemplate.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+cd \temp
+ng new material --routing=true --style=scss
+cd material
+ng add @angular/material
