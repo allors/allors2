05fe58285505966d8778fbc0a5c9ab3175de3c2a
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-11-19 14:12:09 +0600

    Updated copyright text and namespace usage directive
    Signed-off-by: Jubayer Arefin <jubayerarefin@users.noreply.github.com>

--- Base/Database/TestPopulation/Base/Factories/Common/OrganisationExtensions.cs
+++ Base/Database/TestPopulation/Base/Factories/Common/OrganisationExtensions.cs
@@ -1,14 +1,14 @@
-// <copyright file="Setup.cs" company="Allors bvba">
+// <copyright file="OrganisationExtensions.cs" company="Allors bvba">
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Allors
 {
+    using Allors.Domain;
+    using Allors.Domain.TestPopulation;
     using Bogus;
-    using Domain;
-    using Domain.TestPopulation;
-    using Person = Domain.Person;
+    using Person = Allors.Domain.Person;
 
     public static class OrganisationExtensions
     {
