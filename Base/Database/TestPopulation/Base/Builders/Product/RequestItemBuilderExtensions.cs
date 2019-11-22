75ac13cf1fa0046b7395ca901000565394ed809f
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-11-22 09:40:01 +0600

    Update for Issue #7
    - Added RequestItemBuilderExtensions.cs
    - Updated RequestItem.cs accordingly (setting quantity to one)
    
    Signed-off-by: Jubayer Arefin <jubayerarefin@users.noreply.github.com>

---
+++ Base/Database/TestPopulation/Base/Builders/Product/RequestItemBuilderExtensions.cs
@@ -0,0 +1,45 @@
+// <copyright file="RequestItemBuilderExtensions.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>Defines the MediaTests type.</summary>
+
+namespace Allors.Domain.TestPopulation
+{
+    public static partial class RequestItemBuilderExtensions
+    {
+        public static RequestItemBuilder WithSerializedDefaults(this RequestItemBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var finishedGood = new UnifiedGoodBuilder(@this.Session).WithSerialisedDefaults(internalOrganisation).Build();
+
+            @this.WithDescription(faker.Lorem.Words().ToString());
+            @this.WithComment(faker.Lorem.Words().ToString());
+            @this.WithInternalComment(faker.Lorem.Words().ToString());
+            @this.WithMaximumAllowedPrice(faker.Random.UInt());
+            @this.WithProduct(finishedGood).Build();
+            @this.WithRequiredByDate(@this.Session.Now().AddDays(7)).Build();
+            @this.WithSerialisedItem(finishedGood.SerialisedItems.First);
+
+            return @this;
+        }
+
+        public static RequestItemBuilder WithNonSerializedDefaults(this RequestItemBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var finishedGood = new UnifiedGoodBuilder(@this.Session).WithNonSerialisedDefaults(internalOrganisation).Build();
+
+            @this.WithDescription(faker.Lorem.Words().ToString());
+            @this.WithComment(faker.Lorem.Words().ToString());
+            @this.WithInternalComment(faker.Lorem.Words().ToString());
+            @this.WithMaximumAllowedPrice(faker.Random.UInt());
+            @this.WithQuantity(faker.Random.UShort());
+            @this.WithProduct(finishedGood).Build();
+            @this.WithRequiredByDate(@this.Session.Now().AddDays(7)).Build();
+
+            return @this;
+        }
+    }
+}
