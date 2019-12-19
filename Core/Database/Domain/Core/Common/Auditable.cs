12c35e8d8fe3da9c5135bee602595e2d8291ae2b
Koen Van Exem <koen@vanexem.be>
2019-12-19 19:27:21 +0100

    Fixed Auditable PostDerive

--- Core/Database/Domain/Core/Common/Auditable.cs
+++ Core/Database/Domain/Core/Common/Auditable.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Auditable.cs" company="Allors bvba">
+// <copyright file="Auditable.cs" company="Allors bvba">
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -7,7 +7,7 @@ namespace Allors.Domain
 {
     public static class AuditableExtension
     {
-        public static void BaseOnDerive(this Auditable @this, ObjectOnDerive method)
+        public static void CoreOnDerive(this Auditable @this, ObjectOnDerive method)
         {
             var derivation = method.Derivation;
             var user = @this.Strategy.Session.GetUser();
