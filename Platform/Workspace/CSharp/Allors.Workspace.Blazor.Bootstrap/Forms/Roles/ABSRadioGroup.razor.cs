38878ca88c683f376caac9da78d7e3cfa68ccdb2
Koen Van Exem <koen@vanexem.be>
2020-01-06 13:46:02 +0100

    update Blazor RoleField Name and OptionNames

--- Platform/Workspace/CSharp/Allors.Workspace.Blazor.Bootstrap/Forms/Roles/ABSRadioGroup.razor.cs
+++ Platform/Workspace/CSharp/Allors.Workspace.Blazor.Bootstrap/Forms/Roles/ABSRadioGroup.razor.cs
@@ -2,6 +2,7 @@ namespace Allors.Workspace.Blazor.Bootstrap.Forms.Roles
 {
     using System;
     using Microsoft.AspNetCore.Components;
+    using Microsoft.Extensions.Options;
 
     public class ABSRadioGroupBase : RoleField
     {
@@ -10,5 +11,7 @@ namespace Allors.Workspace.Blazor.Bootstrap.Forms.Roles
 
         [Parameter]
         public Func<ISessionObject, string> DisplayOption { get; set; } = o => o.ToString();
+
+        public string OptionName(ISessionObject option) => this.Name + "_" + option.Id.ToString().Replace("-", "_");
     }
 }
