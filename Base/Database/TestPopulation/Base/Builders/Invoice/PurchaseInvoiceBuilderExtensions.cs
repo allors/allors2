83886bb633f908c290fd5e344b6f37dd6ff6c5c2
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-03-27 18:36:54 +0600

    Update for #7
    Builder Extension Update:
    
     - Removed unnecessary usage of `.build()`
     - Moved all Invoice related builders to `Invoice` folder
    
    Signed-off-by: Jubayer Arefin <jubayerarefin@users.noreply.github.com>

---
+++ Base/Database/TestPopulation/Base/Builders/Invoice/PurchaseInvoiceBuilderExtensions.cs
@@ -0,0 +1,43 @@
+// <copyright file="PurchaseInvoiceBuilderExtensions.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>Defines the MediaTests type.</summary>
+
+namespace Allors.Domain.TestPopulation
+{
+    using System.Linq;
+
+    public static partial class PurchaseInvoiceBuilderExtensions
+    {
+        public static PurchaseInvoiceBuilder WithDefaults(this PurchaseInvoiceBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var quoteItem = new QuoteItemBuilder(@this.Session).WithSerializedDefaults(internalOrganisation).Build();
+            var customer = faker.Random.ListItem(internalOrganisation.ActiveCustomers);
+            var postalAddress = new PostalAddressBuilder(@this.Session).WithDefaults().Build();
+            var salesInvoiceType = faker.Random.ListItem(@this.Session.Extent<SalesInvoiceType>());
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithBilledFrom(internalOrganisation);
+            @this.WithBilledFromContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBilledFromContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithDescription(faker.Lorem.Sentence());
+            @this.WithComment(faker.Lorem.Sentence());
+            @this.WithInternalComment(faker.Lorem.Sentence());
+            @this.WithBillToEndCustomer(customer);
+            @this.WithBillToEndCustomerContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToEndCustomer(customer);
+            @this.WithShipToEndCustomerAddress(postalAddress);
+            @this.WithShipToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToCustomer(customer);
+            @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
+
+            return @this;
+        }
+    }
+}
---
+++ Base/Database/TestPopulation/Base/Builders/Invoice/SalesInvoiceBuilderExtensions.cs
@@ -0,0 +1,192 @@
+// <copyright file="SalesInvoiceBuilderExtensions.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>Defines the MediaTests type.</summary>
+
+namespace Allors.Domain.TestPopulation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class SalesInvoiceBuilderExtensions
+    {
+        public static SalesInvoiceBuilder WithSalesInternalInvoiceDefaults(this SalesInvoiceBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var internalOrganisations = @this.Session.Extent<Organisation>();
+
+            var otherInternalOrganization = internalOrganisations.Except(new List<Organisation> { internalOrganisation }).FirstOrDefault();
+
+            var endCustomer = faker.Random.ListItem(internalOrganisation.ActiveCustomers);
+            var salesInvoiceItem = new SalesInvoiceItemBuilder(@this.Session).WithSerializedProductDefaults(internalOrganisation).Build();
+
+            var salesInvoiceType = faker.Random.ListItem(@this.Session.Extent<SalesInvoiceType>());
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithCustomerReference(faker.Random.String(16).ToUpper());
+            @this.WithDescription(faker.Random.Words(10));
+            @this.WithBilledFrom(internalOrganisation);
+            @this.WithBilledFromContactMechanism(internalOrganisation.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBilledFromContactPerson(internalOrganisation.CurrentContacts.FirstOrDefault());
+            @this.WithDescription(faker.Lorem.Sentence());
+            @this.WithComment(faker.Lorem.Sentence());
+            @this.WithInternalComment(faker.Lorem.Sentence());
+            @this.WithBillToCustomer(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithBillToContactMechanism(otherInternalOrganization.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToContactPerson(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithBillToEndCustomer(endCustomer);
+            @this.WithBillToEndCustomerContactMechanism(endCustomer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToEndCustomerContactPerson(endCustomer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToEndCustomer(endCustomer);
+            @this.WithShipToEndCustomerAddress(endCustomer.ShippingAddress);
+            @this.WithShipToEndCustomerContactPerson(endCustomer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToCustomer(otherInternalOrganization);
+            @this.WithShipToAddress(otherInternalOrganization.ShippingAddress);
+            @this.WithShipToContactPerson(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithSalesInvoiceType(salesInvoiceType);
+            @this.WithTotalListPrice(faker.Random.Decimal());
+            @this.WithPaymentMethod(paymentMethod);
+
+            // TODO: WIP
+            /*@this.WithSalesInvoiceItem(salesInvoiceItem).Build();
+            @this.WithSalesInvoiceItem(salesInvoiceItem).Build();*/
+            @this.WithAdvancePayment(faker.Random.Decimal());
+            @this.WithPaymentDays(faker.Random.Int(7, 30));
+            @this.WithIsRepeatingInvoice(faker.Random.Bool());
+            @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
+
+            return @this;
+        }
+
+        public static SalesInvoiceBuilder WithSalesExternalB2BInvoiceDefaults(this SalesInvoiceBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var customer = internalOrganisation.ActiveCustomers.Where(v => v.GetType().Name == typeof(Organisation).Name).FirstOrDefault();
+            var salesInvoiceItem = new SalesInvoiceItemBuilder(@this.Session).WithSerializedProductDefaults(internalOrganisation).Build();
+
+            var salesInvoiceType = faker.Random.ListItem(@this.Session.Extent<SalesInvoiceType>());
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithCustomerReference(faker.Random.String(16).ToUpper());
+            @this.WithDescription(faker.Random.Words(10));
+            @this.WithBilledFrom(internalOrganisation);
+            @this.WithBilledFromContactMechanism(internalOrganisation.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBilledFromContactPerson(internalOrganisation.CurrentContacts.FirstOrDefault());
+            @this.WithDescription(faker.Lorem.Sentence());
+            @this.WithComment(faker.Lorem.Sentence());
+            @this.WithInternalComment(faker.Lorem.Sentence());
+            @this.WithBillToCustomer(customer);
+            @this.WithBillToContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToCustomer(customer);
+            @this.WithShipToAddress(customer.ShippingAddress);
+            @this.WithShipToContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithSalesInvoiceType(salesInvoiceType);
+            @this.WithTotalListPrice(faker.Random.Decimal());
+            @this.WithPaymentMethod(paymentMethod);
+
+            // TODO: WIP
+            /*@this.WithSalesInvoiceItem(salesInvoiceItem).Build();
+            @this.WithSalesInvoiceItem(salesInvoiceItem).Build();*/
+            @this.WithAdvancePayment(faker.Random.Decimal());
+            @this.WithPaymentDays(faker.Random.Int(7, 30));
+            @this.WithIsRepeatingInvoice(faker.Random.Bool());
+            @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
+
+            return @this;
+        }
+
+        public static SalesInvoiceBuilder WithSalesExternalB2CInvoiceDefaults(this SalesInvoiceBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var customer = internalOrganisation.ActiveCustomers.Where(v => v.GetType().Name == typeof(Person).Name).FirstOrDefault();
+            var salesInvoiceItem = new SalesInvoiceItemBuilder(@this.Session).WithSerializedProductDefaults(internalOrganisation).Build();
+
+            var salesInvoiceType = faker.Random.ListItem(@this.Session.Extent<SalesInvoiceType>());
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithCustomerReference(faker.Random.String(16).ToUpper());
+            @this.WithDescription(faker.Random.Words(10));
+            @this.WithBilledFrom(internalOrganisation);
+            @this.WithBilledFromContactMechanism(internalOrganisation.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBilledFromContactPerson(internalOrganisation.CurrentContacts.FirstOrDefault());
+            @this.WithDescription(faker.Lorem.Sentence());
+            @this.WithComment(faker.Lorem.Sentence());
+            @this.WithInternalComment(faker.Lorem.Sentence());
+            @this.WithBillToCustomer(customer);
+            @this.WithBillToContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithShipToCustomer(customer);
+            @this.WithShipToAddress(customer.ShippingAddress);
+            @this.WithSalesInvoiceType(salesInvoiceType);
+            @this.WithTotalListPrice(faker.Random.Decimal());
+            @this.WithPaymentMethod(paymentMethod);
+
+            // TODO: WIP
+            /*@this.WithSalesInvoiceItem(salesInvoiceItem).Build();
+            @this.WithSalesInvoiceItem(salesInvoiceItem).Build();*/
+            @this.WithAdvancePayment(faker.Random.Decimal());
+            @this.WithPaymentDays(faker.Random.Int(7, 30));
+            @this.WithIsRepeatingInvoice(faker.Random.Bool());
+            @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
+
+            return @this;
+        }
+
+        public static SalesInvoiceBuilder WithCreditNoteDefaults(this SalesInvoiceBuilder @this, Organisation internalOrganisation)
+        {
+            var faker = @this.Session.Faker();
+
+            var customer = faker.Random.ListItem(internalOrganisation.ActiveCustomers);
+            var salesInvoiceItem = new SalesInvoiceItemBuilder(@this.Session).WithSerializedProductDefaults(internalOrganisation).Build();
+
+            var salesInvoiceType = faker.Random.ListItem(@this.Session.Extent<SalesInvoiceType>());
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithCustomerReference(faker.Random.String(16).ToUpper());
+            @this.WithDescription(faker.Random.Words(10));
+            @this.WithBilledFrom(internalOrganisation);
+            @this.WithBilledFromContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBilledFromContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithDescription(faker.Lorem.Sentence());
+            @this.WithComment(faker.Lorem.Sentence());
+            @this.WithInternalComment(faker.Lorem.Sentence());
+            @this.WithBillToCustomer(customer.CurrentContacts.FirstOrDefault());
+            @this.WithBillToContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithBillToEndCustomer(customer);
+            @this.WithBillToEndCustomerContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToEndCustomer(customer);
+            @this.WithShipToEndCustomerAddress(customer.ShippingAddress);
+            @this.WithShipToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToCustomer(customer);
+            @this.WithShipToAddress(customer.ShippingAddress);
+            @this.WithShipToContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithSalesInvoiceType(salesInvoiceType);
+            @this.WithTotalListPrice(faker.Random.Decimal());
+            @this.WithPaymentMethod(paymentMethod);
+
+            // TODO: WIP
+            /*@this.WithSalesInvoiceItem(salesInvoiceItem).Build();
+            @this.WithSalesInvoiceItem(salesInvoiceItem).Build();*/
+            @this.WithAdvancePayment(faker.Random.Decimal());
+            @this.WithPaymentDays(faker.Random.Int(7, 30));
+            @this.WithIsRepeatingInvoice(faker.Random.Bool());
+            @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
+            @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
+
+            return @this;
+        }
+    }
+}
