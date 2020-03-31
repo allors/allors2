98638551a07b249a33d42f4f4aba0d953a565fa5
Koen Van Exem <koen@vanexem.be>
2020-03-31 17:57:40 +0200

    npgsql connection string

--- Platform/Database/Adapters/Tests.Static/Static/sql/npgsql/Profile.cs
+++ Platform/Database/Adapters/Tests.Static/Static/sql/npgsql/Profile.cs
@@ -42,12 +42,12 @@ namespace Allors.Database.Adapters.Npgsql
             }
         }
 
-        //protected string ConnectionString => (this.pgServer != null) ?
-        //    $"Server=localhost; Port={this.pgServer.PgPort}; User Id=postgres; Password=test; Database=postgres; Pooling=false; Timeout=300; CommandTimeout=300" :
-        //    $"Server=localhost; User Id=allors; Password=allors; Database=adapters; Pooling=false; Timeout=300; CommandTimeout=300";
+        protected string ConnectionString => (this.pgServer != null) ?
+            $"Server=localhost; Port={this.pgServer.PgPort}; User Id=postgres; Password=test; Database=postgres; Pooling=false; Timeout=300; CommandTimeout=300" :
+            $"Server=localhost; User Id=allors; Password=allors; Database=adapters; Pooling=false; Timeout=300; CommandTimeout=300";
 
-        protected string ConnectionString =>
-            $"Server=localhost; User Id=allors; Password=allors; Database=adapters; Pooling=false";
+        //protected string ConnectionString =>
+        //    $"Server=localhost; User Id=allors; Password=allors; Database=adapters; Pooling=false";
 
         public IDatabase CreateDatabase(IMetaPopulation metaPopulation, bool init)
         {
