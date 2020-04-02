2493f3b5cbbe82839fb4b0069777581c63ad9fb4
martienvanknippenberg <martien@dipu.com>
2020-04-02 13:07:43 +0200

    setup

---
+++ Base/Database/Domain/Base/Common/Singletons.cs
@@ -0,0 +1,18 @@
+// <copyright file="Singletons.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Domain
+{
+    using Allors.Meta;
+
+    public partial class Singletons
+    {
+        protected override void BasePrepare(Setup setup)
+        {
+            setup.AddDependency(this.ObjectType, M.SerialisedItemAssignedOn.ObjectType);
+            setup.AddDependency(this.ObjectType, M.SerialisedItemSoldOn.ObjectType);
+        }
+    }
+}
