9609eb32228515234d1fb2cc553711430299f101
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-04-06 16:59:00 +0600

    Fix for Task #180 Aviaco
    Change formatting as used in EUROPE
    
    Signed-off-by: Jubayer Arefin <jubayerarefin@users.noreply.github.com>

--- Base/Database/Domain/Base/Print/ProductQuote/QuoteModel.cs
+++ Base/Database/Domain/Base/Print/ProductQuote/QuoteModel.cs
@@ -1,23 +1,15 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="QuoteModel.cs" company="Allors bvba">
-//   Copyright 2002-2012 Allors bvba.
-// Dual Licensed under
-//   a) the General Public Licence v3 (GPL)
-//   b) the Allors License
-// The GPL License is included in the file gpl.txt.
-// The Allors License is an addendum to your contract.
-// Allors Applications is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// For more information visit http://www.allors.com/legal
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-
 namespace Allors.Domain.Print.ProductQuoteModel
 {
+    using System.Collections.Generic;
+    using System.Globalization;
+
     public class QuoteModel
     {
         public QuoteModel(Quote quote, Dictionary<string, byte[]> imageByImageName)
@@ -30,11 +22,11 @@ namespace Allors.Domain.Print.ProductQuoteModel
 
             // TODO: Where does the currency come from?
             var currency = "€";
-            this.SubTotal = quote.TotalBasePrice.ToString("0.00") + " " + currency;
-            this.TotalExVat = quote.TotalExVat.ToString("0.00") + " " + currency;
+            this.SubTotal = quote.TotalBasePrice.ToString("N2", new CultureInfo("nl-BE")) + " " + currency;
+            this.TotalExVat = quote.TotalExVat.ToString("N2", new CultureInfo("nl-BE")) + " " + currency;
             this.VatCharge = quote.VatRegime?.VatRate?.Rate.ToString("n2");
-            this.TotalVat = quote.TotalVat.ToString("0.00") + " " + currency;
-            this.TotalIncVat = quote.TotalIncVat.ToString("0.00") + " " + currency;
+            this.TotalVat = quote.TotalVat.ToString("N2", new CultureInfo("nl-BE")) + " " + currency;
+            this.TotalIncVat = quote.TotalIncVat.ToString("N2", new CultureInfo("nl-BE")) + " " + currency;
         }
 
         public string Description { get; }
