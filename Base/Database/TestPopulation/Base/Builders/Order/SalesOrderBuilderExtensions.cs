1f9978b02515b2c7f1844c48d3386d5a569ae5cd
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-04-08 18:24:49 +0600

    Update for #7
    Builder Extension Update:
    
     - Completed SalesOrderBuilderExtensions.cs
     - Completed SalesOrderItemBuilderExtensions.cs
    
    Signed-off-by: Jubayer Arefin <jubayerarefin@users.noreply.github.com>

--- Base/Database/TestPopulation/Base/Builders/Order/SalesOrderBuilderExtensions.cs
+++ Base/Database/TestPopulation/Base/Builders/Order/SalesOrderBuilderExtensions.cs
@@ -2,10 +2,12 @@
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
-// <summary>Defines the MediaTests type.</summary>
+// <summary></summary>
 
 namespace Allors.Domain.TestPopulation
 {
+    using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
 
     public static partial class SalesOrderBuilderExtensions
@@ -14,32 +16,38 @@ namespace Allors.Domain.TestPopulation
         {
             var faker = @this.Session.Faker();
 
-            var quoteItem = new QuoteItemBuilder(@this.Session).WithSerializedDefaults(internalOrganisation).Build();
-            var customer = faker.Random.ListItem(internalOrganisation.ActiveCustomers);
-            var postalAddress = new PostalAddressBuilder(@this.Session).WithDefaults().Build();
+            var internalOrganisations = @this.Session.Extent<Organisation>();
 
+            var otherInternalOrganization = internalOrganisations.Except(new List<Organisation> { internalOrganisation }).FirstOrDefault();
+
+            var endCustomer = faker.Random.ListItem(internalOrganisation.ActiveCustomers);
+            var salesOrderItem_NonGSE = new SalesOrderItemBuilder(@this.Session).WithDefaults(internalOrganisation).Build();
+            var salesOrderItem_GSE = new SalesOrderItemBuilder(@this.Session).WithGSEDefaults(internalOrganisation).Build();
+
+            var paymentMethod = faker.Random.ListItem(@this.Session.Extent<PaymentMethod>());
+
+            @this.WithCustomerReference(faker.Random.String(16).ToUpper(CultureInfo.CurrentCulture));
             @this.WithTakenBy(internalOrganisation);
-            @this.WithTakenByContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
-            @this.WithTakenByContactPerson(customer.CurrentContacts.FirstOrDefault());
+            @this.WithTakenByContactMechanism(internalOrganisation.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithTakenByContactPerson(internalOrganisation.CurrentContacts.FirstOrDefault());
             @this.WithDescription(faker.Lorem.Sentence());
-            @this.WithComment(faker.Lorem.Sentence()).Build();
+            @this.WithComment(faker.Lorem.Sentence());
             @this.WithInternalComment(faker.Lorem.Sentence());
-            @this.WithShipToCustomer(customer);
-            @this.WithShipToContactPerson(customer.CurrentContacts.FirstOrDefault());
-            @this.WithBillToCustomer(customer.CurrentContacts.FirstOrDefault());
-            @this.WithBillToContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
-            @this.WithBillToContactPerson(customer.CurrentContacts.FirstOrDefault());
-            @this.WithBillToEndCustomer(customer);
-            @this.WithBillToEndCustomerContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
-            @this.WithBillToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
-            @this.WithShipToEndCustomer(customer);
-            @this.WithShipToEndCustomerAddress(postalAddress);
-            @this.WithShipToEndCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
-            @this.WithPlacingCustomer(customer);
-            @this.WithPlacingCustomerContactMechanism(customer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
-            @this.WithPlacingCustomerContactPerson(customer.CurrentContacts.FirstOrDefault());
-            @this.WithOriginFacility(new FacilityBuilder(@this.Session).WithDefaults(internalOrganisation).Build());
-            @this.WithPartiallyShip(faker.Random.Bool());
+            @this.WithBillToCustomer(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithBillToContactMechanism(otherInternalOrganization.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToContactPerson(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithBillToEndCustomer(endCustomer);
+            @this.WithBillToEndCustomerContactMechanism(endCustomer.CurrentPartyContactMechanisms.Select(v => v.ContactMechanism).FirstOrDefault());
+            @this.WithBillToEndCustomerContactPerson(endCustomer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToEndCustomer(endCustomer);
+            @this.WithShipToEndCustomerAddress(endCustomer.ShippingAddress);
+            @this.WithShipToEndCustomerContactPerson(endCustomer.CurrentContacts.FirstOrDefault());
+            @this.WithShipToCustomer(otherInternalOrganization);
+            @this.WithShipToAddress(otherInternalOrganization.ShippingAddress);
+            @this.WithShipToContactPerson(otherInternalOrganization.CurrentContacts.FirstOrDefault());
+            @this.WithPaymentMethod(paymentMethod);
+            @this.WithSalesOrderItem(salesOrderItem_NonGSE).Build();
+            @this.WithSalesOrderItem(salesOrderItem_GSE).Build();
             @this.WithSalesTerm(new IncoTermBuilder(@this.Session).WithDefaults().Build());
             @this.WithSalesTerm(new InvoiceTermBuilder(@this.Session).WithDefaults().Build());
             @this.WithSalesTerm(new OrderTermBuilder(@this.Session).WithDefaults().Build());
