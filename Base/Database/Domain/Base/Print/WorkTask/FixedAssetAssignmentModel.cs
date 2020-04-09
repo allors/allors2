d5cc95f0d5b5033981a82137765ff0a789e0c3fe
martienvanknippenberg <martien@dipu.com>
2020-04-09 09:20:17 +0200

    setup

--- Base/Database/Domain/Base/Print/WorkTask/FixedAssetAssignmentModel.cs
+++ Base/Database/Domain/Base/Print/WorkTask/FixedAssetAssignmentModel.cs
@@ -3,12 +3,17 @@
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Linq;
+using Allors.Meta;
+
 namespace Allors.Domain.Print.WorkTaskModel
 {
     public class FixedAssetAssignmentModel
     {
         public FixedAssetAssignmentModel(WorkEffortFixedAssetAssignment assignment)
         {
+            var session = assignment.Strategy.Session;
+
             this.Name = assignment.FixedAsset?.Name;
             this.Comment = assignment.FixedAsset?.Comment;
 
@@ -19,6 +24,10 @@ namespace Allors.Domain.Print.WorkTaskModel
                 this.SerialNumber = serialisedItem.SerialNumber;
                 this.Brand = serialisedItem.PartWhereSerialisedItem?.Brand?.Name;
                 this.Model = serialisedItem.PartWhereSerialisedItem?.Model?.Name;
+
+                var hoursType = new SerialisedItemCharacteristicTypes(session).FindBy(M.SerialisedItemCharacteristicType.Name, "Operating Hours");
+                var hoursCharacteristic = serialisedItem.SerialisedItemCharacteristics.FirstOrDefault(v => v.SerialisedItemCharacteristicType.Equals(hoursType));
+                this.Hours = $"{hoursCharacteristic?.Value} {hoursType?.UnitOfMeasure?.Abbreviation}";
             }
         }
 
@@ -33,6 +42,8 @@ namespace Allors.Domain.Print.WorkTaskModel
         public string Brand { get; }
 
         public string Model { get; }
+
+        public string Hours { get; }
 
         public string Comment { get; }
     }
