3ac991ac8a6e09252e9473970d9feb85d0426913
Koen Van Exem <koen@vanexem.be>
2020-05-08 11:06:26 +0200

    angular locales

--- Base/Workspace/Typescript/Intranet/custom-webpack.config.js
+++ Base/Workspace/Typescript/Intranet/custom-webpack.config.js
@@ -3,7 +3,7 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = {
   plugins: [
     new MomentLocalesPlugin({
-      localesToKeep: ['en']
+      localesToKeep: ['nl']
     })
   ]
 };
