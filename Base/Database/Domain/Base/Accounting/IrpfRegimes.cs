2975d103667e0ad9dc09b858264e0da51c9b4ef4
martienvanknippenberg <martien@dipu.com>
2020-06-29 09:59:29 +0200

    IRPF regime

--- Base/Database/Domain/Base/Accounting/IrpfRegimes.cs
+++ Base/Database/Domain/Base/Accounting/IrpfRegimes.cs
@@ -10,14 +10,11 @@ namespace Allors.Domain
 
     public partial class IrpfRegimes
     {
-        public static readonly Guid Assessable10Id = new Guid("a82edb4e-dc92-4864-96fe-26ec6d1ef914");
-        public static readonly Guid ExemptId = new Guid("82986030-5E18-43c1-8CBE-9832ACD4151D");
+        public static readonly Guid Assessable19Id = new Guid("a82edb4e-dc92-4864-96fe-26ec6d1ef914");
 
         private UniquelyIdentifiableSticky<IrpfRegime> cache;
 
-        public IrpfRegime Assessable10 => this.Cache[Assessable10Id];
-
-        public IrpfRegime Exempt => this.Cache[ExemptId];
+        public IrpfRegime Assessable19 => this.Cache[Assessable19Id];
 
         private UniquelyIdentifiableSticky<IrpfRegime> Cache => this.cache ??= new UniquelyIdentifiableSticky<IrpfRegime>(this.Session);
 
@@ -28,27 +25,18 @@ namespace Allors.Domain
 
         protected override void BaseSetup(Setup setup)
         {
-            var irpfRate0 = new IrpfRates(this.Session).Ten;
-            var irpfRate10 = new IrpfRates(this.Session).Ten;
+            var irpfRate19 = new IrpfRates(this.Session).nineteen;
 
             var dutchLocale = new Locales(this.Session).DutchNetherlands;
 
             var merge = this.Cache.Merger().Action();
             var localisedName = new LocalisedTextAccessor(this.Meta.LocalisedNames);
 
-            merge(Assessable10Id, v =>
+            merge(Assessable19Id, v =>
             {
-                v.Name = "IRPF Assessable 10%";
-                localisedName.Set(v, dutchLocale, "IRPF-plichtig 10%");
-                v.IrpfRate = irpfRate10;
-                v.IsActive = true;
-            });
-
-            merge(ExemptId, v =>
-            {
-                v.Name = "Exempt";
-                localisedName.Set(v, dutchLocale, "Vrijgesteld");
-                v.IrpfRate = irpfRate0;
+                v.Name = "IRPF Assessable 19%";
+                localisedName.Set(v, dutchLocale, "IRPF-plichtig 19%");
+                v.IrpfRate = irpfRate19;
                 v.IsActive = true;
             });
         }
