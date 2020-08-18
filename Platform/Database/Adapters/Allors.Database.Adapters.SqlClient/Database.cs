9504b1982594507966546c2d0be1d78f7804b39a
Koen Van Exem <koen@vanexem.be>
2020-08-18 12:35:18 +0200

    Domain Derivations

--- Platform/Database/Adapters/Allors.Database.Adapters.SqlClient/Database.cs
+++ Platform/Database/Adapters/Allors.Database.Adapters.SqlClient/Database.cs
@@ -86,6 +86,8 @@ namespace Allors.Database.Adapters.SqlClient
             }
 
             this.SchemaName = (configuration.SchemaName ?? "allors").ToLowerInvariant();
+
+            this.DomainDerivationById = new Dictionary<Guid, IDomainDerivation>();
         }
 
         public event ObjectNotLoadedEventHandler ObjectNotLoaded;
@@ -175,6 +177,8 @@ namespace Allors.Database.Adapters.SqlClient
                 return this.mapping;
             }
         }
+
+        public IDictionary<Guid, IDomainDerivation> DomainDerivationById { get; }
 
         public ISession CreateSession()
         {
