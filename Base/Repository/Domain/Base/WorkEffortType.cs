444f779771c2d052ec898767bc333a7a06901139
martienvanknippenberg <martien@dipu.com>
2020-09-21 09:57:50 +0200

    change Repository/Domain/Base/WorkEffortType.cs

--- Base/Repository/Domain/Base/WorkEffortType.cs
+++ Base/Repository/Domain/Base/WorkEffortType.cs
@@ -20,13 +20,32 @@ namespace Allors.Repository
         #endregion
 
         #region Allors
+        [Id("1ecbdb5f-5fb7-492d-9e08-df3aa322371d")]
+        [AssociationId("18658209-b7cc-4e0e-9cb3-0238d213042d")]
+        [RoleId("33368ac1-1b2e-4bea-8e3e-e13430babdc0")]
+        #endregion
+        [Required]
+        [Workspace]
+        public string Name { get; set; }
+
+        #region Allors
+        [Id("93cfed3d-ae24-4a07-becf-34cdc3cdef3e")]
+        [AssociationId("958d3cdc-0dbe-4ce6-81c7-492825727ada")]
+        [RoleId("d0fadb8a-4891-4caf-a010-6197676cfd54")]
+        #endregion
+        [Required]
+        [Workspace]
+        [Size(-1)]
+        public string Description { get; set; }
+
+        #region Allors
         [Id("5ce1a600-62a9-4d2c-bfb5-bfe374b2099f")]
         [AssociationId("4d892148-3583-46a2-b68d-895274b9ea7a")]
         [RoleId("fa8657ae-5132-4f37-aba0-4f95c4b1df1e")]
         #endregion
         [Multiplicity(Multiplicity.OneToMany)]
         [Indexed]
-
+        [Workspace]
         public WorkEffortFixedAssetStandard[] WorkEffortFixedAssetStandards { get; set; }
 
         #region Allors
@@ -36,48 +55,7 @@ namespace Allors.Repository
         #endregion
         [Multiplicity(Multiplicity.OneToMany)]
         [Indexed]
-
         public WorkEffortGoodStandard[] WorkEffortGoodStandards { get; set; }
-
-        #region Allors
-        [Id("89eef4e3-eda7-4336-91cb-ce7a7e96521f")]
-        [AssociationId("4b61f74f-db7c-4733-b6f5-d485e432a16e")]
-        [RoleId("8b9f019b-e79e-4282-9ee1-9bc652fd6817")]
-        #endregion
-        [Multiplicity(Multiplicity.ManyToMany)]
-        [Indexed]
-
-        public WorkEffortType[] Children { get; set; }
-
-        #region Allors
-        [Id("8d9f51b5-2c8d-4a25-a45e-c79542a09434")]
-        [AssociationId("687e9909-0efa-4a04-b705-96d93547458a")]
-        [RoleId("da4648c1-b495-4555-8fa3-c4ba8141e67d")]
-        #endregion
-        [Multiplicity(Multiplicity.ManyToOne)]
-        [Indexed]
-
-        public FixedAsset FixedAssetToRepair { get; set; }
-
-        #region Allors
-        [Id("93cfed3d-ae24-4a07-becf-34cdc3cdef3e")]
-        [AssociationId("958d3cdc-0dbe-4ce6-81c7-492825727ada")]
-        [RoleId("d0fadb8a-4891-4caf-a010-6197676cfd54")]
-        #endregion
-        [Required]
-        [Size(-1)]
-
-        public string Description { get; set; }
-
-        #region Allors
-        [Id("b6d68eff-8a3a-473f-bb4e-9bc46808bde0")]
-        [AssociationId("1996225b-a372-44ad-b00a-b257a355d756")]
-        [RoleId("2c704b66-922a-4c5c-81fb-973545230501")]
-        #endregion
-        [Multiplicity(Multiplicity.ManyToMany)]
-        [Indexed]
-
-        public WorkEffortType[] Dependencies { get; set; }
 
         #region Allors
         [Id("d51d620e-250e-4492-8926-c8535fad19ec")]
@@ -86,7 +64,7 @@ namespace Allors.Repository
         #endregion
         [Multiplicity(Multiplicity.OneToMany)]
         [Indexed]
-
+        [Workspace]
         public WorkEffortPartStandard[] WorkEffortPartStandards { get; set; }
 
         #region Allors
@@ -96,8 +74,38 @@ namespace Allors.Repository
         #endregion
         [Multiplicity(Multiplicity.OneToMany)]
         [Indexed]
-
+        [Workspace]
         public WorkEffortSkillStandard[] WorkEffortSkillStandards { get; set; }
+
+        #region Allors
+        [Id("89eef4e3-eda7-4336-91cb-ce7a7e96521f")]
+        [AssociationId("4b61f74f-db7c-4733-b6f5-d485e432a16e")]
+        [RoleId("8b9f019b-e79e-4282-9ee1-9bc652fd6817")]
+        #endregion
+        [Multiplicity(Multiplicity.ManyToMany)]
+        [Indexed]
+        [Workspace]
+        public WorkEffortType[] Children { get; set; }
+
+        #region Allors
+        [Id("8d9f51b5-2c8d-4a25-a45e-c79542a09434")]
+        [AssociationId("687e9909-0efa-4a04-b705-96d93547458a")]
+        [RoleId("da4648c1-b495-4555-8fa3-c4ba8141e67d")]
+        #endregion
+        [Multiplicity(Multiplicity.ManyToOne)]
+        [Indexed]
+        [Workspace]
+        public FixedAsset FixedAssetToRepair { get; set; }
+
+        #region Allors
+        [Id("b6d68eff-8a3a-473f-bb4e-9bc46808bde0")]
+        [AssociationId("1996225b-a372-44ad-b00a-b257a355d756")]
+        [RoleId("2c704b66-922a-4c5c-81fb-973545230501")]
+        #endregion
+        [Multiplicity(Multiplicity.ManyToMany)]
+        [Indexed]
+        [Workspace]
+        public WorkEffortType[] Dependencies { get; set; }
 
         #region Allors
         [Id("df1fa89e-25e2-4b72-a928-67fa2c95ad70")]
@@ -106,6 +114,7 @@ namespace Allors.Repository
         #endregion
         [Precision(19)]
         [Scale(2)]
+        [Workspace]
         public decimal StandardWorkHours { get; set; }
 
         #region Allors
@@ -115,7 +124,7 @@ namespace Allors.Repository
         #endregion
         [Multiplicity(Multiplicity.ManyToOne)]
         [Indexed]
-
+        [Workspace]
         public Product ProductToProduce { get; set; }
 
         #region Allors
@@ -125,7 +134,7 @@ namespace Allors.Repository
         #endregion
         [Multiplicity(Multiplicity.ManyToOne)]
         [Indexed]
-
+        [Workspace]
         public Deliverable DeliverableToProduce { get; set; }
 
         #region inherited methods
