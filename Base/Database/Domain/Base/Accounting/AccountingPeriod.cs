6b2606513c711adfbdb19d818983fbb902ff30cb
martienvanknippenberg <martien@dipu.com>
2021-02-25 14:58:43 +0100

    asdf

--- Base/Database/Domain/Base/Accounting/AccountingPeriod.cs
+++ Base/Database/Domain/Base/Accounting/AccountingPeriod.cs
@@ -2,7 +2,8 @@
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
-
+ 
+ 
 namespace Allors.Domain
 {
     using System.Text;
