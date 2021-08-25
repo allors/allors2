6c9493bd287ffbe50c21409d1d95d8780cab0744
martienvanknippenberg <martien@dipu.com>
2021-08-25 15:29:49 +0200

    NumberFormats

--- Core/Workspace/CSharp/Excel/Excel/Allors/Core/Controls/NumberFormats.cs
+++ Core/Workspace/CSharp/Excel/Excel/Allors/Core/Controls/NumberFormats.cs
@@ -3,8 +3,10 @@ namespace Excel.Allors
     public static class NumberFormats
     {        
         public const string NoDecimal = "### ### ### ###";
+        public const string Integer = "0";
         public const string OneDecimal = "0.0#";
         public const string TwoDecimals = "0.00#";
+        public const string ThreeDecimals = "0.000#";
         public const string PercentageOneDecimal = "0.0%";
         public const string PercentageTwoDecimal = "0.00%";
         public const string ISODateTime = "YYYY/MM/DD";     
